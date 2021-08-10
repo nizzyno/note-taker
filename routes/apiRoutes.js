2675b57a744622ab00c1d79253d100a92e6ec914
Noah <mcbeasty11.nh@icloud.com>
2021-08-10 07:00:46 -0500

    fixed server

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,41 @@
 const router = require('express').Router();
-let { notes } = require('../db/db.json');
-const uniqid = require('uniqid');
-const { write } = require('../../notes');
+const {
+  findById,
+  createNewNote,
+  validateNote,
+  removeNote,
+} = require('../lib/notes.js');
+const { notes } = require('../db/db.json');
+const uuid = require('uuid');
 
 router.get('/notes', (req, res) => {
-  res.json(notes);
+  let results = notes;
+  res.json(results);
+});
+
+router.get('/notes:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 router.post('/notes', (req, res) => {
-  req.body.id = uniqid();
+  req.body.id = notes.length.toString();
 
-  res.json(write(req.body, notes));
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+router.delete('/notes/:id', (req, res) => {
+  note = removeNote(req.params.id, notes);
+  res.json(note);
 });
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,5 @@
-const path = require('path');
 const router = require('express').Router();
+const path = require('path');
 
 router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
